6aa8e33b4741eb8355c7b92079074224bdf05b49
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-01-05 21:55:10 -0800

    Move user-packages to home-manager

--- users/jerry.nix
+++ users/jerry.nix
@@ -5,12 +5,5 @@
     isNormalUser = true;
     description = "Jerry";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      git
-      fastfetch
-      firefox
-      flatpak
-      rofi-wayland
-    ];
   };
 }
