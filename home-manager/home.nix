38277d871f0909ecadf166b25fb60fe8bf442f96
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-01-07 20:04:12 -0800

    Organize NixOS configs to have both flake and home manager

---
+++ home-manager/home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "jerry";
+  home.homeDirectory = "/home/jerry";
+
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    git
+    firefox
+    flatpak
+    rofi-wayland
+  ];
+
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
