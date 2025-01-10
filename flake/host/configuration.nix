371d53b6ce5c317268df1e5d251e4bdfa99125be
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-01-09 23:40:24 -0800

    Clean up configuration.nix by removing some comments

--- flake/host/configuration.nix
+++ flake/host/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -34,10 +30,6 @@
   };
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -109,7 +101,7 @@
     sbctl
     dbus
     home-manager
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     alacritty
   ];
