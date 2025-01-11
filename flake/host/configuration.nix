5024d691ad2b7e11f134a9007e603809fe9f76bf
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-01-11 14:09:22 -0800

    Remove sbctl and home-manager in configuration.nix

--- flake/host/configuration.nix
+++ flake/host/configuration.nix
@@ -98,9 +98,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    sbctl
     dbus
-    home-manager
     vim
     wget
     alacritty
