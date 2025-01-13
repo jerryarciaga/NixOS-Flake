7c46c5efa938161abe2b1a6a3a9d6efa0baedb25
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-01-12 21:54:58 -0800

    Add privacy-related preferences

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -1,5 +1,15 @@
 { config, pkgs, ... }:
 
+let
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+in
 {
   programs.firefox = {
     enable = true;
@@ -40,6 +50,29 @@
         Autoplay = {
           Default = "block-audio-video";
         };
+      };
+
+      Preferences = { 
+          "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+          "extensions.pocket.enabled" = lock-false;
+          "extensions.screenshots.disabled" = lock-true;
+          "browser.topsites.contile.enabled" = lock-false;
+          "browser.formfill.enable" = lock-false;
+          "browser.search.suggest.enabled" = lock-false;
+          "browser.search.suggest.enabled.private" = lock-false;
+          "browser.urlbar.suggest.searches" = lock-false;
+          "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+          "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+          "security.insecure_connection_text.enabled" = lock-true;
+          "privacy.globalprivacycontrol.enabled" = lock-true;
       };
     };
   };
