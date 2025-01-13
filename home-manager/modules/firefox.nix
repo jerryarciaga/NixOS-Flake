5e50b326c16249fe48d1cf05c647be0e2bcf41b4
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-01-12 21:33:18 -0800

    Add more firefox policies

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -5,10 +5,42 @@
     enable = true;
 
     policies = {
+      # Telemetry and tracking protection
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
+
+      # Autofill
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
+
+      # Addons
       DisablePocket = true;
       DisplayBookmarksToolbar = "always";
+      FirefoxSuggest = {
+        WebSuggestions = false;
+        SponsoredSuggestions = false;
+        ImproveSuggest = false;
+        Locked = true;
+      };
+      HardwareAcceleration = true;
+
+      # Logins and password management
+      PasswordManagerEnabled = false;
+      OfferToSaveLoginsDefault = false;
+
+      # Permissions
+      Permissions = {
+        Autoplay = {
+          Default = "block-audio-video";
+        };
+      };
     };
   };
 }
