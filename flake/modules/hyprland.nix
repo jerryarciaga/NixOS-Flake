0b50111f7c6c26006fe4876c2c182cffc17fad35
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-02-09 12:30:04 -0800

    Add PAM module for Hyprlock setup

--- flake/modules/hyprland.nix
+++ flake/modules/hyprland.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
     brightnessctl
   ];
+  security.pam.services.hyprlock = {};
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
