6681ad7ca7fbc92107564edfe1b7172602edec4a
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-02-09 12:28:27 -0800

    Initial config for Waybar Calendar

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -13,17 +13,15 @@
         position = "left";
         width = 10;
         modules-left = [ 
-          "hyprland/mode"
+          "hyprland/workspaces"
         ];
         modules-center = [
-          "hyprland/workspaces"
+          "clock"
         ];
         modules-right = [
           "tray"
           "backlight"
           "pulseaudio"
-          "clock"
-          "date"
         ];
 
         "hyprland/workspaces" = {
@@ -32,8 +30,22 @@
         };
 
         "clock" = {
-          "format" = "{:%H:%M\n%d %b}";
-          "format-alt" = "{:%H:%M\n%a, %d %b}";
+          "format" = "{:%H\n%M}";
+          "tooltip-format" = "<tt><small>{calendar}</small></tt>";
+          "calendar" =  {
+            "mode"          = "year";
+            "mode-mon-col"  = 3;
+            "weeks-pos"     = "right";
+            "on-scroll"     = 1;
+            "format" = {
+              "months" =     "<span color='#ffead3'><b>{}</b></span>";
+              "days" =       "<span color='#ecc6d9'><b>{}</b></span>";
+              "weeks" =      "<span color='#99ffdd'><b>W{}</b></span>";
+              "weekdays" =   "<span color='#ffcc66'><b>{}</b></span>";
+              "today" =      "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          #"format-alt" = "{%H\n%M\n%a\n%d\n%b}";
         };
 
       };
