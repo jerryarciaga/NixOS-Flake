b1a514946fa0b062f7f8bade51bad44aaefa74a9
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-02-09 00:46:07 -0800

    Install brightnessctl for brightness control

--- flake/modules/hyprland.nix
+++ flake/modules/hyprland.nix
@@ -1,6 +1,9 @@
 { pkgs, lib, inputs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    brightnessctl
+  ];
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
