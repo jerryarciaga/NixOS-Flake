46c242a725142169e19630482e1dde70ce7b2186
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-02-11 19:22:02 -0800

    Simplify install script

--- home-manager/install.sh
+++ home-manager/install.sh
@@ -9,36 +9,13 @@ HOME_MANAGER=$PWD
 CONFIG_DIR="$HOME/.config/home-manager"
 CONFIG_FILES=`ls $HOME_MANAGER/*.nix`
 
-# Start from scratch
-if [[ -a $CONFIG_DIR/flake.lock ]]; then
-  rm $CONFIG_DIR/*.nix
-elif [[ -L $CONFIG_DIR ]]; then
+# Start from scratch if there is home manager installed
+if [[ -L $CONFIG_DIR ]]; then
   echo "Removing link $CONFIG_DIR..."
   rm $CONFIG_DIR
-  mkdir $CONFIG_DIR
 elif [[ -d $CONFIG_DIR ]]; then
   echo "Removing directory $CONFIG_DIR..."
   rm -rf $CONFIG_DIR
-  mkdir $CONFIG_DIR
-elif [[ ! -d $CONFIG_DIR ]]; then
-  echo "Creating directory $CONFIG_DIR..."
-  mkdir $CONFIG_DIR
 fi
 
-# Create/recreate links to editor
-for config_file in $CONFIG_FILES; do
-  ln -sf $config_file $CONFIG_DIR/.
-  echo "Link to $config_file"
-done
-
-# Relink modules
-if [[ -d $CONFIG_DIR/modules ]]; then
-  echo "Removing directory $CONFIG_DIR/modules..."
-  rm -rf $CONFIG_DIR/modules
-elif [[ -a $CONFIG_DIR/modules ]]; then
-  rm $CONFIG_DIR/modules
-  echo "Removing file/link $CONFIG_DIR/modules..."
-fi
-
-echo "Creating link $HOME_MANAGER/modules -> $CONFIG_DIR/modules..."
-ln -sf $HOME_MANAGER/modules $CONFIG_DIR/modules
+ln -sf $HOME_MANAGER $CONFIG_DIR
