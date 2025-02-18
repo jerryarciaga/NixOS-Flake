2a78d13d2ca6afcf4a8fad53e885633f06b12dab
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-02-18 15:00:06 -0800

    Removed font awesome, added nerd fonts symbols

--- home-manager/modules/fonts.nix
+++ home-manager/modules/fonts.nix
@@ -6,9 +6,9 @@
   home.packages = with pkgs; [
     noto-fonts
     noto-fonts-emoji
-    font-awesome
     gyre-fonts
     cascadia-code
     nerd-fonts.ubuntu
+    nerd-fonts.symbols-only
   ];
 }
