8c12839dcce4072f1a4dcd141f9b950c87d6edd4
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-05-03 23:07:39 -0700

    Enable stylix

--- home-manager/desktop/theme.nix
+++ home-manager/desktop/theme.nix
@@ -1,14 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  catppuccin.gtk = {
+  stylix = {
     enable = true;
-    flavor = "mocha";
-    accent = "blue";
-    icon = {
-      enable = true;
-      accent = "blue";
-      flavor = "mocha";
-    };
+    base16Scheme =
+      "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
   };
 }
