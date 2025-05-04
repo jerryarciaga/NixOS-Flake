ad7d3d3a8b73c2d85e29e8187c5e529dc42f9f3d
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-05-03 23:48:42 -0700

    Disable stylix for already-configured applications

--- home-manager/desktop/theme.nix
+++ home-manager/desktop/theme.nix
@@ -5,5 +5,10 @@
     enable = true;
     base16Scheme =
       "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    targets = {
+      alacritty.enable = false;
+      firefox.enable = false;
+      waybar.enable = false;
+    };
   };
 }
