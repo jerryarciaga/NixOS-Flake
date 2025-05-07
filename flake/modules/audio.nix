4dfdf2c677e9954407dafa4814a8181306508ac1
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-05-06 21:04:46 -0700

    Remove Bluetooth support

--- flake/modules/audio.nix
+++ flake/modules/audio.nix
@@ -1,25 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   services.pipewire = {
     enable = true;
     audio.enable = true;
     pulse.enable = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    pulseaudio
-    pavucontrol
-  ];
-
-  # Bluetooth configuration
-  # See https://nixos.wiki/wiki/PipeWire#Bluetooth_Configuration
-  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
-    "monitor.bluez.properties" = {
-        "bluez5.enable-sbc-xq" = true;
-        "bluez5.enable-msbc" = true;
-        "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
-    };
   };
 }
