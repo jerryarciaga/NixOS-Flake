4a61fc92a98ece5ebec3255fa29cb5569acea533
Jerry Arciaga <jerryarciaga11@gmail.com>
2026-01-10 14:43:29 -0800

    Enable Bluetooth for computers

--- system/modules/audio.nix
+++ system/modules/audio.nix
@@ -10,4 +10,18 @@
   environment.systemPackages = with pkgs; [
     pavucontrol
   ];
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = false;
+      };
+      Policy = {
+        AutoEnable = false;
+      };
+    };
+  };
 }
