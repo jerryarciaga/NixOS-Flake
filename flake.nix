799f49e7cf0090c44b9d65e3e30e8924e58d75ec
Jerry Arciaga <jerryarciaga11@gmail.com>
2026-02-13 14:49:59 -0800

    Move home-manager to root directory of repo and fixed neovim config

--- flake.nix
+++ flake.nix
@@ -3,17 +3,39 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # System inputs
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Home Manager inputs
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    catppuccin = {
+      url = "github:catppuccin/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     lanzaboote,
+    home-manager,
+    stylix,
+    catppuccin,
     ...
   } @ inputs:
   
@@ -37,6 +59,8 @@
       # Apps
       ./system/modules/yubikey.nix
     ];
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in
   {
     nixosConfigurations = {
@@ -107,7 +131,46 @@
           ./system/modules/intel_graphics.nix
         ];
       };
-
     };
+
+    homeConfigurations."jerry" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [
+        ./home-manager/home.nix
+
+        # Hyprland dots
+        ./home-manager/desktop/theme.nix
+        ./home-manager/desktop/hyprland.nix
+        ./home-manager/desktop/hyprpaper.nix
+        ./home-manager/desktop/hyprlock.nix
+        ./home-manager/desktop/waybar.nix
+        ./home-manager/desktop/wofi.nix
+
+        # Theme
+        stylix.homeModules.stylix
+        catppuccin.homeModules.catppuccin
+
+        # App configs
+        ./home-manager/modules/alacritty.nix
+        ./home-manager/modules/git.nix
+        ./home-manager/modules/neovim.nix
+        ./home-manager/modules/zathura.nix
+        ./home-manager/modules/firefox.nix
+        ./home-manager/modules/fonts.nix
+
+        # Services
+        ./home-manager/modules/ssh.nix
+      ];
+
+      # Use extraSpecialArgs
+      # to pass through arguments to home.nix
+      extraSpecialArgs = {
+        inherit inputs;
+      };
+    };
+
   };
 }
