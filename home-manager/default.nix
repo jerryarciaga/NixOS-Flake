12fb8549d1d7c9bf61fa2fe03a3f1ff4516ad49b
Jerry Arciaga <jerryarciaga11@gmail.com>
2026-04-15 23:31:01 -0700

    Created wrapper function to define default home-manager configuration

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -3,47 +3,50 @@
 let
   system = "x86_64-linux";
   pkgs = inputs.nixpkgs.legacyPackages.${system};
+
+  mkHome = { modules ? [ ] }:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [
+        ./home.nix
+
+        # Hyprland dots
+        ./desktop/theme.nix
+        ./desktop/hyprland.nix
+        ./desktop/hyprpaper.nix
+        ./desktop/hyprlock.nix
+        ./desktop/waybar.nix
+        ./desktop/wofi.nix
+
+        # Theme
+        inputs.stylix.homeModules.stylix
+        inputs.catppuccin.homeModules.catppuccin
+
+        # App configs
+        ./modules/alacritty.nix
+        ./modules/git.nix
+        ./modules/neovim.nix
+        ./modules/zathura.nix
+        ./modules/firefox.nix
+        ./modules/fonts.nix
+
+        # Services
+        ./modules/ssh.nix
+      ] ++ modules;
+
+      # Use extraSpecialArgs
+      # to pass through arguments to home.nix
+      extraSpecialArgs = {
+        inherit inputs;
+      };
+    };
 in
 
 {
 
-  jerry = inputs.home-manager.lib.homeManagerConfiguration {
-    inherit pkgs;
-
-    # Specify your home configuration modules here, for example,
-    # the path to your home.nix.
-    modules = [
-      ./home.nix
-
-      # Hyprland dots
-      ./desktop/theme.nix
-      ./desktop/hyprland.nix
-      ./desktop/hyprpaper.nix
-      ./desktop/hyprlock.nix
-      ./desktop/waybar.nix
-      ./desktop/wofi.nix
-
-      # Theme
-      inputs.stylix.homeModules.stylix
-      inputs.catppuccin.homeModules.catppuccin
-
-      # App configs
-      ./modules/alacritty.nix
-      ./modules/git.nix
-      ./modules/neovim.nix
-      ./modules/zathura.nix
-      ./modules/firefox.nix
-      ./modules/fonts.nix
-
-      # Services
-      ./modules/ssh.nix
-    ];
-
-    # Use extraSpecialArgs
-    # to pass through arguments to home.nix
-    extraSpecialArgs = {
-      inherit inputs;
-    };
-  };
+  jerry = mkHome { };
 
 }
