540baed6ee739d9d2d5c10e46fb852d431629eef
Jerry Arciaga <jerryarciaga11@gmail.com>
2026-04-23 22:55:51 -0700

    Add functions to bashrc

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -43,6 +43,40 @@
       # include .profile if it exists
       [[ -f ~/.profile ]] && . ~/.profile
     '';
+    bashrcExtra = ''
+      nixos_rebuild_switch() {
+        if [[ -d /build ]]; then
+          export TMPDIR="/build"
+        fi
+
+        if [[ -d $HOME/.dotfiles/nixos ]]; then
+          sudo nixos-rebuild switch --flake $HOME/.dotfiles/nixos
+        else
+          sudo nixos-rebuild switch --flake .
+        fi
+
+        if [[ -v TMPDIR ]]; then
+          unset TMPDIR
+        fi
+      }
+
+      nixos_cleanup() {
+        sudo nix store optimise
+        sudo nix-collect-garbage --delete-older-than 2d
+      }
+
+      nix_build_iso() {
+        if [[ -d $HOME/.dotfiles/nixos ]]; then
+          FLAKE="$HOME/.dotfiles/nixos"
+        else
+          FLAKE=$PWD
+        fi
+        nix build \
+          $FLAKE/#nixosConfigurations.coffeemaker.config.system.build.isoImage \
+          -o coffeemaker
+        unset FLAKE
+      }
+    '';
   };
 
   # Let Home Manager install and manage itself.
