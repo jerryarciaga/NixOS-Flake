e1b0973288911ef244f962110d74be913c67131a
Jerry Arciaga <jerryarciaga11@gmail.com>
2026-07-03 18:51:43 -0700

    Fix niri module file

--- home-manager/desktop/niri.nix
+++ home-manager/desktop/niri.nix
@@ -2,11 +2,10 @@
  
 {
 
-  xdg.configFile.'niri/config.kdl'.source = ./config/niri/config.kdl;
+  xdg.configFile."niri/config.kdl".source = ./config/niri/config.kdl;
 
-  programs.alacritty.enable = true; # Super+T in the default setting (terminal)
-  programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
   programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
+  programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
   programs.waybar.enable = true; # launch on startup in the default setting (bar)
   services.mako.enable = true; # notification daemon
   services.swayidle.enable = true; # idle management daemon
