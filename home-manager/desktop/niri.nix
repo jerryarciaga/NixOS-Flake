bb7e807e327c0a982988716d80fa187c8f52e4c5
Jerry Arciaga <jerryarciaga11@gmail.com>
2026-07-05 18:51:05 -0700

    Remove unneeded program waybar

--- home-manager/desktop/niri.nix
+++ home-manager/desktop/niri.nix
@@ -3,10 +3,10 @@
 {
 
   xdg.configFile."niri/config.kdl".source = ./config/niri/config.kdl;
+  xdg.configFile."wallpaper".source = ./wallpaper;
 
   programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
   programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
-  programs.waybar.enable = true; # launch on startup in the default setting (bar)
   services.mako.enable = true; # notification daemon
   services.swayidle.enable = true; # idle management daemon
   services.polkit-gnome.enable = true; # polkit
