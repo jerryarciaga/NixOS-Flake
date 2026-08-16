d23dce57d27ef0abc6f375d01b8a56245cbf1f11
Jerry Arciaga <jerryarciaga11@gmail.com>
2026-08-15 20:25:35 -0700

    Add containers and tweak privacy settings in Firefox

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   lock-false = {
@@ -20,6 +20,60 @@ in
   programs.firefox = {
     enable = true;
     configPath = "${config.xdg.configHome}/mozilla/firefox";
+
+    profiles.default = {
+      name = "default";
+      id = 0;
+      isDefault = true;
+      search = {
+        force = true;
+        default = "ddg";
+        order = [ "ddg" "google" ];
+      };
+      containersForce = true;
+      containers = {
+        "1. Personal" = {
+          id = 1;
+          color = "blue";
+          icon = "fingerprint";
+        };
+        "2. School" = {
+          id = 2;
+          color = "yellow";
+          icon = "briefcase";
+        };
+        "3. Work" = {
+          id = 3;
+          color = "turquoise";
+          icon = "briefcase";
+        };
+        "4. Banking" = {
+          id = 4;
+          color = "red";
+          icon = "dollar";
+        };
+        "5. Shopping" = {
+          id = 5;
+          color = "orange";
+          icon = "cart";
+        };
+        "6. Social" = {
+          id = 6;
+          color = "red";
+          icon = "pet";
+        };
+        "7. Gaming" = {
+          id = 7;
+          color = "red";
+          icon = "gift";
+        };
+        "8. Other" = {
+          id = 8;
+          color = "red";
+          icon = "chill";
+        };
+      };
+    };
 
     policies = {
       # Telemetry and tracking protection
@@ -68,25 +122,23 @@ in
       };
 
       Preferences = { 
+          "sidebar.verticalTabs" = lock-true;
+          "sidebar.revamp" = lock-true;
+          "devtools.toolbox.sidebar.width" = { Value = 1000; Status = "locked"; };
+          "sidebar.verticalTabs.dragToPinPromo.dismissed" = lock-true;
+          "sidebar.visibility" = { Value = "always-show"; Status = "locked"; };
           "layout.css.devPixelsPerPx" = "-1.0";
           "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
           "extensions.pocket.enabled" = lock-false;
           "extensions.screenshots.disabled" = lock-true;
+          "browser.theme.content-theme" = { Value = 2; Status = "locked"; };
           "browser.topsites.contile.enabled" = lock-false;
           "browser.formfill.enable" = lock-false;
           "browser.search.suggest.enabled" = lock-false;
           "browser.search.suggest.enabled.private" = lock-false;
           "browser.urlbar.suggest.searches" = lock-false;
           "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
-          "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
-          "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
-          "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
-          "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
-          "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
-          "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
-          "browser.newtabpage.activity-stream.showSponsored" = lock-false;
-          "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+          # "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
           "security.insecure_connection_text.enabled" = lock-true;
           "privacy.globalprivacycontrol.enabled" = lock-true;
           # Disable AI Tools
